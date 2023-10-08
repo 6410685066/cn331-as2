dad8477dd5736aee95e686e75a86b7fcab947615
Korawat Plainsuwan <6410685066@cn103>
2023-10-08 21:02:16 +0700

    Initial commit

--- subjects/admin.py
+++ subjects/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import subject
 from users.models import ListRegSubject
 
-class ListRegSubjectInline(admin.TabularInline):  # หรือใช้ StackedInline แทน
+class ListRegSubjectInline(admin.TabularInline):  
     model = ListRegSubject
-    extra = 1  # กำหนดจำนวนช่องในแบบฟอร์ม
+    extra = 1  
 
 class adminsubject(admin.ModelAdmin):
     list_display = ['subjectName', 'subjectID', 'remaining_class', 'max_class', 'subjectStatus']
